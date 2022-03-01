2de1cddaa5c7d332959918a0cc7444660e956580
Peng Jia <pengjia1110@gmail.com>
2022-03-01 14:40:49 +0800

    Update test.sh

--- demo/test.sh
+++ demo/test.sh
@@ -1,3 +1,3 @@
-msisensor-rna train -i ./train.csv -m demo.pkl -t CRC
-msisensor-rna show -m demo.pkl -g genes.list
+msisensor-rna train -i ./train.csv -m demo.pkl -t CRC 
+msisensor-rna show -m demo.pkl -g genes.list 
 msisensor-rna detection -i ./test.csv -m demo.pkl -o test.output.csv
